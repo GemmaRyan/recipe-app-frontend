5863ed78c0a39eaf5bbf8e904a6b631d878c3958
GemmaRyan <s00250864@atu.ie>
2025-11-27 00:07:34 +0000

    adding the addRecipe files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './home/home';
 import { Allrecipes } from './allrecipes/allrecipes';
 import { RecipeDetails } from './recipe-details/recipe-details';
 import { EditRecipes } from './edit-recipes/edit-recipes';
+import { AddRecipe } from './add-recipe/add-recipe';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,6 +11,6 @@ export const routes: Routes = [
   { path: 'recipes', component: Allrecipes },
   { path: 'recipe/:id', component: RecipeDetails },
   { path: 'edit/:id', component: EditRecipes },
-//   { path: 'create', component: EditRecipesComponent },
+  { path: 'create', component: AddRecipe },
   { path: '**', redirectTo: '/home' }
 ];
