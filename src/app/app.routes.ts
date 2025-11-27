715bb9099f38d567fd8d048367ef67bb1438be38
GemmaRyan <s00250864@atu.ie>
2025-11-27 00:05:48 +0000

    fixing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Allrecipes } from './allrecipes/allrecipes';
+import { RecipeDetails } from './recipe-details/recipe-details';
+import { EditRecipes } from './edit-recipes/edit-recipes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'recipes', component: Allrecipes },
+  { path: 'recipe/:id', component: RecipeDetails },
+  { path: 'edit/:id', component: EditRecipes },
+//   { path: 'create', component: EditRecipesComponent },
+  { path: '**', redirectTo: '/home' }
+];
